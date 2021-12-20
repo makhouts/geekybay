c8b1da6c040ede9ce66374ac8cb2fa18db459e88
Naama Kaplan <nkaplany@gmail.com>
2021-12-20 16:51:41 +0100

    orderRouter set up

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,24 +1,10 @@
 import express from 'express'
-// import bcrypt from 'bcrypt'
 
 const router = express.Router();
 
 //Get all users
 router.get('/', (req,res) => {
-    res.send('allusers')
+    res.send('allOrders')
 })
-
-//Get user by id
-
-
-//Create user
-
-
-//Update user
-
-
-//Delete user
-
-
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import userRouter from './routes/userRouter.js'
+import orderRouter from './routes/orderRouter.js'
 
 const app = express();
 
 app.use('/users', userRouter)
+app.use('/orders', orderRouter)
 
 app.listen(3000)
