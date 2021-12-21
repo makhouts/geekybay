07ff6f6cccd77c0cd9a570da876a381b4fcf6a48
minjie0501 <minjie.fan@icloud.com>
2021-12-21 15:56:17 +0100

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import express from 'express'
-import userRouter from './routes/userRouter.js'
-import productRouter from './routes/productRouter.js'
-
-const app = express();
-
-app.use('/users', userRouter)
-app.use('/products', productRouter)
-=======
 if (process.env.NODE_ENV !== "production") {
   const dotenv = await import("dotenv");
   dotenv.config();
@@ -23,7 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use("/users", userRouter);
->>>>>>> 8e9e38a1bf6e7fe878d91479354e35eb5721a5b1
 
 
 
