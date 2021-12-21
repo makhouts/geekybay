d7c08a0421c56aa4e8fb5b4ba4bd6317d31ec0ec
Naama Kaplan <nkaplany@gmail.com>
2021-12-21 10:03:20 +0100

    getting all orders via orderRouter possible

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,10 +1,25 @@
-import express from 'express'
+import express from 'express';
+//import connection pool
+import pool from "../helper/dbConnection.js";
 
 const router = express.Router();
 
-//Get all users
+//Get all orders
 router.get('/', (req,res) => {
-    res.send('allOrders')
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query("SELECT * from orders", (err, rows) => {
+            //get connection from the pool and release it back
+            connection.release();
+
+            if (!err) {
+                //request has been processed successfully on the server
+                res.status(200).send(rows);
+            } else {
+                res.status(400).send('Bad request');
+            }
+        })
+    })
 })
 
 export default router
