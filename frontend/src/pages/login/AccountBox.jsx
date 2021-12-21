838b54051259837c154ef9c0db88b35f24351c4f
830503 <lixiaoqi19830503@gmail.com>
2021-12-21 12:22:06 +0100

    added head

--- frontend/src/pages/login/AccountBox.jsx
+++ frontend/src/pages/login/AccountBox.jsx
@@ -5,6 +5,9 @@ export const AccountBox = () => {
   return (
     <div className="BoxContainer">
       <div className="TopContainer">
+        <div className="HeaderContainer">
+          <div className="HeadText">Login</div>
+        </div>
         <div className="BackDrop"></div>
       </div>
     </div>
