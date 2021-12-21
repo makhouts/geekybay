61e2be2044b25fcefba8b5c05aa31c18284f7835
Naama Kaplan <nkaplany@gmail.com>
2021-12-21 11:47:36 +0100

    adding new order with POST functional

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -16,10 +16,44 @@ router.get('/', (req,res) => {
                 //request has been processed successfully on the server
                 res.status(200).send(rows);
             } else {
+                //The request could not be understood by the server due to incorrect syntax. The client SHOULD NOT repeat the request without modifications.
                 res.status(400).send('Bad request');
             }
-        })
-    })
-})
+        });
+    });
+});
+
+//Get order by id
+router.get("/:id", (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query("SELECT * FROM orders WHERE orderid = ?", [req.params.id], (err, rows) => {
+            connection.release();
+            if (!err) {
+                res.status(200).send(rows);
+            } else {
+                res.status(400).send('Bad request')
+            }
+        });
+    });
+});
+
+//add new order
+router.post("/", (req, res) => {
+    pool.getConnection((err, connection) => {
+        const params = req.body;
+        if (err) throw err;
+        connection.query("INSERT INTO orders SET ?", params, (err, rows) => {
+            connection.release();
+            if (!err) {
+                res.status(200).send(rows);
+            } else {
+                res.status(400).send('Bad request')
+            }
+        });
+    });
+});
+
+
 
 export default router
