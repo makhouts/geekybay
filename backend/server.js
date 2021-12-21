3219ada2bd7406543a5a27c15b83e8d534d30138
minjie0501 <minjie.fan@icloud.com>
2021-12-21 14:36:23 +0100

    crud for users

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,12 @@ import userRouter from "./routes/userRouter.js";
 
 const app = express();
 
+import bodyParser from 'body-parser'
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use("/users", userRouter);
+
 
 app.listen(process.env.SERVER_PORT);
