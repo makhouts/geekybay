b37841ec6459053b7ef9b484a499a08a4d6e112a
RedMarkD <reinout.de.bleser@gmail.com>
2021-12-22 09:26:20 +0100

    prepull commit

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
 import pool from "../helper/dbConnection.js";
-
+import multer from "multer";
+import fs from "fs";
+import path from "path";
 
 const router = express.Router();
 
@@ -19,6 +21,11 @@ router.get("/", (req, res) => {
             }
         });
     });
+});
+
+router.get("/2.png", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, "/2.png"));
 });
 
 //Get product by sellerid
@@ -71,6 +78,7 @@ router.get("/product/:productId", (req, res) => {
 //Create product
 
 router.post("/", (req, res) => {
+    const upload = multer({ dest: 'uploads/' })
     console.log(req.body);
     pool.getConnection((err, connection) => {
         if (err) throw err;
@@ -83,6 +91,7 @@ router.post("/", (req, res) => {
                 res.status(400).send('Bad product creation request')
             }
         });
+
     });
 });
 
@@ -118,6 +127,46 @@ router.delete("/:id", (req, res) => {
         });
     });
 });
+//Multer test for implementation: using router and post. multipart form data.
+
+const handleError = (err, res) => {
+    res
+        .status(500)
+        .contentType("text/plain")
+        .end("Oops! Something went wrong!");
+};
+const upload = multer({
+    dest: "backend/Temp",
+    // you might also want to set some limits: https://github.com/expressjs/multer#limits
+} );
+router.post(
+    "/upload",
+    upload.single("file" /* name attribute of <file> element in your form */),
+    (req, res) => {
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, "./uploads/image.png");
+
+        if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+            fs.rename(tempPath, targetPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(200)
+                    .contentType("text/plain")
+                    .end("File uploaded!");
+            });
+        } else {
+            fs.unlink(tempPath, err => {
+                if (err) return handleError(err, res);
+
+                res
+                    .status(403)
+                    .contentType("text/plain")
+                    .end("Only .png files are allowed!");
+            });
+        }
+    }
+);
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,18 @@ import express from 'express'
 import userRouter from './routes/userRouter.js'
 import productRouter from './routes/productRouter.js'
 import bodyParser from 'body-parser'
+import multer from 'multer'
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/users', userRouter)
-app.use('/products', productRouter)
+app.use('/users', userRouter);
+app.use('/products', productRouter);
+app.use(multer)
 
 if (process.env.NODE_ENV !== "production") {
   const dotenv = await import("dotenv");
   dotenv.config();
 }
-
 
 app.listen(process.env.SERVER_PORT);
