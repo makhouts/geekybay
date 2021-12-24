63eb5d7720ffc1e72b2f485933b1eace2d25c8be
Naama Kaplan <nkaplany@gmail.com>
2021-12-24 11:59:06 +0100

    added free shipping to products in database

--- geekyBay.sql
+++ geekyBay.sql
@@ -151,10 +151,10 @@ CREATE TABLE `products` (
   `sellerID` INT NOT NULL,
   `productDescription` text NOT NULL,
   `price` decimal(10,2) NOT NULL,
-<<<<<<< HEAD
   `inStock`INT NOT NULL,
   `visible` boolean NOT NULL,
   `productImg` varchar(255),
+  `freeShipping` BOOL NOT NULL,
   PRIMARY KEY (`productID`),
   KEY `products` (`sellerID`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`sellerID`) REFERENCES `users` (`userID`)
@@ -164,12 +164,12 @@ CREATE TABLE `products` (
 
 /*Data for the table `products` */
 
-insert  into `products`(`productName`, `sellerID`, `productDescription`, `price`,`inStock`,`visible`, `productImg`) values
+insert  into `products`(`productName`, `sellerID`, `productDescription`, `price`,`inStock`,`visible`, `productImg`, `freeShipping`) values
 
-( 'guitar1', '1', 'stringy1', '1000', 5, true, '1.jpg'),
-( 'guitar2', '2', 'stringy2', '2000', 6, true, '2.jpg'),
-( 'guitar3', '3', 'stringy3', '3000', 0, false, '3.jpg'),
-( 'guitar4', '4', 'stringy3', '4000', 10, false, '4.jpg')
+( 'guitar1', '1', 'stringy1', '1000', 5, true, '1.jpg', true),
+( 'guitar2', '2', 'stringy2', '2000', 6, true, '2.jpg', false),
+( 'guitar3', '3', 'stringy3', '3000', 0, false, '3.jpg', true),
+( 'guitar4', '4', 'stringy3', '4000', 10, false, '4.jpg', false)
 ;
 
 
