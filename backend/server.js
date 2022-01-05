15319f8f16f7eec4139fba50a2736ebd75346bbc
minjie0501 <minjie.fan@icloud.com>
2022-01-05 12:17:49 +0100

    done

--- backend/server.js
+++ backend/server.js
@@ -90,7 +90,7 @@ app.use("/pay", paymentRouter);
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
 app.listen(process.env.PORT);
