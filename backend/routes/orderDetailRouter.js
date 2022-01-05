eefe08863db2549af9290758c0380251ffbfd3f6
Naama Kaplan <nkaplany@gmail.com>
2022-01-05 13:33:59 +0100

    fixed orderdetail cancellation

--- backend/routes/orderDetailRouter.js
+++ backend/routes/orderDetailRouter.js
@@ -87,15 +87,16 @@ router.post("/" , validate(orderDetailValidation, {}, {}),(req, res) => {
     });
 });
 
-//cancel order
+//cancel order as seller
 router.put("/cancel/:id", isAuth, (req, res) => {
     pool.getConnection((err, connection) => {
         const orderStatus = "canceled";
         if (err) throw err;
         //params is a request parameter in the url
-        connection.query("UPDATE orderdetails SET orderStatus=? WHERE orderID=? AND sellerID=?", [orderStatus, req.params.id, req.user.userID], (err, rows) => {
+        connection.query("UPDATE orderdetails SET orderStatus=? WHERE orderDetailID=? AND sellerID=?", [orderStatus, req.params.id, req.user.userID], (err, rows) => {
             connection.release();
             if (!err) {
+                console.log(rows);
                 //the request has succeeded and a new resource has been created as a result.
                 res.status(201).send(rows);
             } else {
@@ -103,6 +104,34 @@ router.put("/cancel/:id", isAuth, (req, res) => {
                 res.status(400).send('Bad request')
             }
         });
+    });
+});
+
+//cancel order as buyer (only if logged in)
+router.put("/cancel/buyer/:id", isAuth, (req, res) => {
+    pool.getConnection((err, connection) => {
+        const orderStatus = "canceled";
+        if (err) throw err;
+        connection.query("SELECT orderStatus FROM orderdetails WHERE orderDetailID=?"), [req.params.id], (err, rows) => {
+            connection.release();
+            if (!err) {
+                console.log(rows);
+
+            } else {
+                res.status(400).send('Bad request for existing order status.')
+            }
+        };
+        // //params is a request parameter in the url
+        // connection.query("UPDATE orderdetails SET orderStatus=? WHERE orderID=? AND buyerID=?", [orderStatus, req.params.id, req.user.userID], (err, rows) => {
+        //     connection.release();
+        //     if (!err) {
+        //         //the request has succeeded and a new resource has been created as a result.
+        //         res.status(201).send(rows);
+        //     } else {
+        //         //?
+        //         res.status(400).send('Bad request')
+        //     }
+        // });
     });
 });
 
