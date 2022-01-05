16f29a8a28c256232da9af4e3f5c0fb6603f5fa7
Naama Kaplan <nkaplany@gmail.com>
2022-01-05 15:12:08 +0100

    buyer can cancel pending order

--- backend/routes/orderDetailRouter.js
+++ backend/routes/orderDetailRouter.js
@@ -96,7 +96,7 @@ router.put("/cancel/:id", isAuth, (req, res) => {
         connection.query("UPDATE orderdetails SET orderStatus=? WHERE orderDetailID=? AND sellerID=?", [orderStatus, req.params.id, req.user.userID], (err, rows) => {
             connection.release();
             if (!err) {
-                console.log(rows);
+                //console.log(rows);
                 //the request has succeeded and a new resource has been created as a result.
                 res.status(201).send(rows);
             } else {
@@ -110,28 +110,29 @@ router.put("/cancel/:id", isAuth, (req, res) => {
 //cancel order as buyer (only if logged in)
 router.put("/cancel/buyer/:id", isAuth, (req, res) => {
     pool.getConnection((err, connection) => {
+        const pendingStatus = "pending";
         const orderStatus = "canceled";
         if (err) throw err;
-        connection.query("SELECT orderStatus FROM orderdetails WHERE orderDetailID=?"), [req.params.id], (err, rows) => {
-            connection.release();
+        connection.query("SELECT orderStatus FROM orderdetails WHERE orderDetailID=?", [req.params.id], (err, rows) => {
             if (!err) {
-                console.log(rows);
+                //console.log(rows[0].orderStatus);
+                if (rows[0].orderStatus === pendingStatus) {
+                    connection.query("UPDATE orderdetails SET orderStatus=? WHERE orderDetailID=? AND buyerID=?", [orderStatus, req.params.id, req.user.userID], (err, rows) => {
+                        connection.release();
+                        if (!err) {
+                            console.log(rows);
+                            res.status(201).send(rows);
 
+                        } else {
+                            res.status(400).send('Bad request for canceling order.')
+                        }
+                    });
+                }
             } else {
-                res.status(400).send('Bad request for existing order status.')
+                connection.release();
+                res.status(400).send('Bad request for existing order status.');
             }
-        };
-        // //params is a request parameter in the url
-        // connection.query("UPDATE orderdetails SET orderStatus=? WHERE orderID=? AND buyerID=?", [orderStatus, req.params.id, req.user.userID], (err, rows) => {
-        //     connection.release();
-        //     if (!err) {
-        //         //the request has succeeded and a new resource has been created as a result.
-        //         res.status(201).send(rows);
-        //     } else {
-        //         //?
-        //         res.status(400).send('Bad request')
-        //     }
-        // });
+        });
     });
 });
 
@@ -147,7 +148,7 @@ router.put("/confirm/:id", isAuth, (req, res) => {
                     connection.release();
                     //the request has succeeded and a new resource has been created as a result.
                     if (!err) {
-                        console.log(rows);
+                       // console.log(rows);
                         res.status(200).send(rows);
                         //send mail to buyer
                         Email.orderConfirmationMail(rows[0].emailAddress).catch(console.error);
